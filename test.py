dac39a604c7c85143c1b09f8e103da9a61b066b0
chiefJurist <anthonynnanna004@gmail.com>
2024-07-19 05:17:45 +0100

    ccccccccc

--- test.py
+++ test.py
@@ -65,27 +65,45 @@ binance_futures = ccxt.binanceusdm({
 
 
 
-#Setting Leverage and entering trade
-balance = binance_futures.fetch_balance()['total']['USDT']
-print(balance)
+# #Setting Leverage and entering trade
+# balance = binance_futures.fetch_balance()['total']['USDT']
+# print(balance)
 
-leverage = binance_futures.set_leverage(3, 'ETH/USDT:USDT')
-print(leverage)
+# leverage = binance_futures.set_leverage(3, 'ETH/USDT:USDT')
+# print(leverage)
 
-current_price = binance_futures.fetch_ticker('ETH/USDT:USDT')['last']
-print(current_price)
+# current_price = binance_futures.fetch_ticker('ETH/USDT:USDT')['last']
+# print(current_price)
 
-amount = balance * 3 / current_price
-print(amount)
+# amount = balance * 3 / current_price
+# print(amount)
 
-createOrder = binance_futures.create_market_sell_order("ETH/USDT:USDT", amount)
-print("Order:", createOrder)
+# createOrder = binance_futures.create_market_sell_order("ETH/USDT:USDT", amount)
+# print("Order:", createOrder)
 
-time.sleep(20)
+# time.sleep(20)
 
-positions = binance_futures.fetch_positions_risk()
-print("position:", positions)
+# positions = binance_futures.fetch_positions_risk()
+# print("position:", positions)
 
-for position in positions:
-    closeOrder = binance_futures.create_market_buy_order('ETH/USDT:USDT', abs(float(position['info']['positionAmt'])))
-    print("Order:", closeOrder)
+# for position in positions:
+#     closeOrder = binance_futures.create_market_buy_order('ETH/USDT:USDT', abs(float(position['info']['positionAmt'])))
+#     print("Order:", closeOrder)
+
+
+
+
+#Setting STOCHRISI
+def fetch_OHLCV(symbol, timeframe, limit=500):
+    bars = binance_futures.fetch_ohlcv(symbol, timeframe, limit=limit)
+    df = pd.DataFrame(bars, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
+    return df
+
+def calculate_stoch_rsi(df):
+    stoch_rsi_k, stoch_rsi_d = ta.STOCHRSI(df['close'], timeperiod=14)
+    return stoch_rsi_k, stoch_rsi_d
+
+df = fetch_OHLCV('ETH/USDT:USDT', '5m')
+stoch_rsi_k, stoch_rsi_d = calculate_stoch_rsi(df)
+print(stoch_rsi_k, stoch_rsi_d)
