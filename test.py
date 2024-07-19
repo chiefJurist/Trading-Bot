680936eb2acfd441423a21fce984444de12d6f79
chiefJurist <anthonynnanna004@gmail.com>
2024-07-19 05:21:36 +0100

    xxxxxxxx

--- test.py
+++ test.py
@@ -94,7 +94,7 @@ binance_futures = ccxt.binanceusdm({
 
 
 #Setting STOCHRISI
-def fetch_OHLCV(symbol, timeframe, limit=500):
+def fetch_OHLCV(symbol, timeframe, limit=1000):
     bars = binance_futures.fetch_ohlcv(symbol, timeframe, limit=limit)
     df = pd.DataFrame(bars, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
@@ -106,4 +106,4 @@ def calculate_stoch_rsi(df):
 
 df = fetch_OHLCV('ETH/USDT:USDT', '5m')
 stoch_rsi_k, stoch_rsi_d = calculate_stoch_rsi(df)
-print(stoch_rsi_k, stoch_rsi_d)
+print(stoch_rsi_k[999], stoch_rsi_d[999])
