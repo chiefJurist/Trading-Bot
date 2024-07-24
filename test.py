79e45da8c7e0a37ae37536ed128fa5a251a9b1da
chiefJurist <anthonynnanna004@gmail.com>
2024-07-24 14:34:25 +0100

    sss

--- test.py
+++ test.py
@@ -38,7 +38,7 @@ def calculate_indicators(df):
     #STOCHASTIC OSILLATOR
     rsi = ta.RSI(df['close'].values, timeperiod=14)
     k, d = ta.STOCH(rsi, rsi, rsi, fastk_period=14, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
-    print(k,d)
+    print(k[500],d[500])
 
 df = fetch_OHLCV('1000PEPE/USDT', '5m')
 calculate_indicators(df)
