b3ee1c909c7baca21caa9208035335d4c99e3f03
chiefJurist <anthonynnanna004@gmail.com>
2024-07-25 17:36:17 +0100

    s

--- test.py
+++ test.py
@@ -50,5 +50,5 @@ def calculate_indicators(df):
 df = fetch_OHLCV('1000PEPE/USDT', '3m')
 calculate_indicators(df)
 
-orders = binance_futures.fetch_closed_orders('ETH/USDT:USDT')
+orders = binance_futures.fetch_closed_orders('1000BONK/USDT:USDT')
 print(orders[0])
