dce6d22a37b1ad68b71cd863ada3f05c518581ab
chiefJurist <anthonynnanna004@gmail.com>
2024-07-25 14:38:41 +0100

    dd

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ def calculate_indicators(df):
     ema1 = ta.EMA(df['close'], timeperiod=50)
     ema2 = ta.EMA(df['close'], timeperiod=100)
     ema3 = ta.EMA(df['close'], timeperiod=200)
-    print(ema1, ema2, ema3)
+    print(ema1.tail(1), ema2.tail(1), ema3.tail(1))
 
 df = fetch_OHLCV('1000PEPE/USDT', '5m')
 calculate_indicators(df)
