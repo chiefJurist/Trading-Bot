52ca87d687551a8ff5f6be471544a19d2b87894d
chiefJurist <anthonynnanna004@gmail.com>
2024-07-29 19:09:19 +0100

    c

--- test.py
+++ test.py
@@ -35,7 +35,7 @@ def calculate_indicators(df):
     df['middle_band'] = middle_band
     df['lower_band'] = lower_band
     print( 'Last two upper band:', upper_band[499])
-    print( 'Last two lower band:', lower_band.tail[499])
+    print( 'Last two lower band:', lower_band[499])
 
     # #STOCHASTIC OSILLATOR
     # rsi = ta.RSI(df['close'].values, timeperiod=14)
