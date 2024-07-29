a755c114a1c4d9ab0eafb989e658c67f92119e80
chiefJurist <anthonynnanna004@gmail.com>
2024-07-29 19:13:51 +0100

    x

--- test.py
+++ test.py
@@ -30,10 +30,10 @@ def fetch_OHLCV(symbol, timeframe):
 
 def calculate_indicators(df):
     #BB
-    df['upper_band'], df['middle_band'], df['lower_band'] = ta.BBANDS(
+    upper_band, middle_band, lower_band = ta.BBANDS(
         df['close'], timeperiod=21, nbdevup=2, nbdevdn=2
     )
-    print(df['upper_band'], df['middle_band'], df['lower_band'])
+    print(upper_band, middle_band, lower_band)
 
     # #STOCHASTIC OSILLATOR
     # rsi = ta.RSI(df['close'].values, timeperiod=14)
