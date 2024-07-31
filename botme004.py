76ca94fa624c0dec37d654275a3dbffed71ee618
chiefJurist <anthonynnanna004@gmail.com>
2024-07-31 06:03:20 +0100

    xx

--- botme004.py
+++ botme004.py
@@ -90,7 +90,7 @@ def manage_futures_positions_and_balance():
     #MAIN TRADING LOGIC
     if len(positions) == 0:
         #Creating order for a golden cross at a good BB
-        if k.tail(1) > (d.tail(1) + 15) and current_price < middleband:      
+        if k[499] > (d[499] + 15) and current_price < middleband:      
             amount = usdt_balance * 10 / current_price
             binance_futures.create_market_buy_order("1000PEPE/USDT:USDT", amount)   
             time.sleep(20) #add a break for safety
@@ -103,7 +103,7 @@ def manage_futures_positions_and_balance():
             binance_futures.create_limit_sell_order('1000PEPE/USDT:USDT', close_amount, target_price)
             time.sleep(10) #add a break for safety
         #Creating order for a death cross at a good ema
-        elif (k.tail(1) + 10) < d.tail(1) and current_price > middleband:
+        elif (k[499] + 10) < d[499] and current_price > middleband:
             amount = usdt_balance * 10 / current_price
             binance_futures.create_market_sell_order("1000PEPE/USDT:USDT", amount)   
             time.sleep(10) #add a break for safety
@@ -133,10 +133,10 @@ def manage_futures_positions_and_balance():
     elif len(positions) > 0 and  len(orders) > 0:
         for position in positions:
             #Exiting Before A Major Loss
-            if position['side'] == 'short' and k.tail(1) > (d.tail(1) + 15):
+            if position['side'] == 'short' and k[499] > (d[499] + 15):
                 close_amount = abs(float(position['info']['positionAmt']))
                 binance_futures.create_market_buy_order('1000PEPE/USDT:USDT', close_amount, target_price)
-            elif position['side'] == 'long' and (k.tail(1) + 10) < d.tail(1):
+            elif position['side'] == 'long' and (k[499] + 10) < d[499]:
                 close_amount = abs(float(position['info']['positionAmt']))
                 binance_futures.create_market_sell_order('1000PEPE/USDT:USDT', close_amount, target_price)
     else:
