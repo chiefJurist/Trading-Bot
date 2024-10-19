cc3c8017861c51dafe2813d383d2301d73c304f9
chiefJurist <anthonynnanna004@gmail.com>
2024-10-19 07:00:26 +0100

    jj

--- error.py
+++ error.py
@@ -42,13 +42,14 @@ def calculate_indicators(df, window=20, num_std_dev=1):
                     slowd_period=3, 
                     slowd_matype=0)
     
+
     #Bollinger Bands
-    upperband, middleband, lowerband = ta.BBANDS(df['close'].values, timeperiod=20, nbdevup=1, nbdevdn=1, matype=0)
-    # rolling_mean = df['close'].rolling(window=window).mean()
-    # rolling_std = df['close'].rolling(window=window).std()
-    # upperband = rolling_mean + (rolling_std * num_std_dev)
-    # middleband = rolling_mean
-    # lowerband = rolling_mean - (rolling_std * num_std_dev)
+    # **Exponential Moving Average (EMA)**
+    ema = ta.EMA(df['close'].values, timeperiod=window)
+    rolling_std = df['close'].rolling(window=window).std()
+    upperband = ema + (rolling_std * num_std_dev)
+    middleband = ema
+    lowerband = ema - (rolling_std * num_std_dev)
 
     return k, d, upperband, middleband, lowerband
 
