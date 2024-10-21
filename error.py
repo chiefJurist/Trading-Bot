5951b8f8396dd76637c3895f9e99c5a493c5f716
chiefJurist <anthonynnanna004@gmail.com>
2024-10-21 04:25:00 +0100

    z\x

--- error.py
+++ error.py
@@ -86,7 +86,7 @@ print(usdt_balance)
 positions = binance_futures.fetch_positions_risk()
 orders = binance_futures.fetch_open_orders('1000PEPE/USDT:USDT')
 current_price = binance_futures.fetch_ticker('1000PEPE/USDT:USDT')['last']
-print(positions)
+print(orders)
 
 for position in positions:
     print(position['side'])
