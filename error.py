914dacc3202be3183d30e8cfbafdc3ac53f62a8d
chiefJurist <anthonynnanna004@gmail.com>
2024-10-21 04:54:26 +0100

    saczc

--- error.py
+++ error.py
@@ -95,4 +95,4 @@ for order in closed_orders[-2:] :
     print (order['info'])
 
 for order in open_orders :
-    binance_futures.cancel_order(order['id'])
+    binance_futures.cancel_order(order['id'], '1000PEPE/USDT:USDT')
