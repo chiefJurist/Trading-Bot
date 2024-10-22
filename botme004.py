08bf974bc7403e1ac852b843b06f3fcb6714b77b
chiefJurist <anthonynnanna004@gmail.com>
2024-10-22 19:25:06 +0100

    ilytrewqASEFJ

--- botme004.py
+++ botme004.py
@@ -166,7 +166,7 @@ def manage_futures_positions_and_balance():
     if k[498] > d[498] and k[497] > d[497]:  
         if last_close > lowerband[498] and second_last_close > lowerband[497] and third_last_close < lowerband[496]:
             try:
-                amount = usdt_balance * 5 / current_price #using half of the capital
+                amount = usdt_balance * 4.9 / current_price #using half of the capital
                 binance_futures.create_order(
                     symbol="1000PEPE/USDT:USDT",  # Symbol for the asset
                     side="BUY",                   # Buy to open a long position
@@ -201,7 +201,7 @@ def manage_futures_positions_and_balance():
     if k[498] < d[498]:  
         if last_close < upperband[498] and second_last_close > upperband[497] :
             try:
-                amount = usdt_balance * 5 / current_price #using half of the capital
+                amount = usdt_balance * 4.9 / current_price #using half of the capital
                 binance_futures.create_order(
                     symbol='1000PEPE/USDT:USDT',  # Symbol for the asset
                     side='SELL',                  # Sell to open a short position
