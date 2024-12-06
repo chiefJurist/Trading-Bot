e4759d1481dcc2d0ce6e4e4e32d2d81dfb4cbea4
chiefJurist <anthonynnanna004@gmail.com>
2024-12-06 12:51:40 +0100

    zx\x

--- test.py
+++ test.py
@@ -54,12 +54,12 @@ def calculate_stoch(df):
 def calculate_bollinger(df, window, num_std_dev):
     # Calculate the moving average (middle band) and round it to 7 significant figures
     middle_band_calc = df['close'].rolling(window=window, min_periods=1).mean()
-    middleband = middle_band_calc.apply(significant_figures)
+    middleband = middle_band_calc#.apply(significant_figures)
     # Calculate the standard deviation and use it to derive the upper and lower bands
     std_dev = df['close'].rolling(window=window, min_periods=1).std()
     # Calculate the upper and lower bands and round them to 6 significant figures
-    upperband = (middle_band_calc + (std_dev * num_std_dev)).apply(significant_figures)
-    lowerband = (middle_band_calc - (std_dev * num_std_dev)).apply(significant_figures)
+    upperband = (middle_band_calc + (std_dev * num_std_dev))#.apply(significant_figures)
+    lowerband = (middle_band_calc - (std_dev * num_std_dev))#.apply(significant_figures)
     
     return upperband, middleband, lowerband
 
@@ -71,7 +71,7 @@ big_df = fetch_OHLCV('ETH/USDT:USDT', '5m')
 k, d = calculate_stoch(df)
 big_k, big_d = calculate_stoch(big_df)
 upperband, middleband, lowerband = calculate_bollinger(df, 20, 0.975)
-big_upperband, big_middleband, big_lowerband = calculate_bollinger(big_df, 20, 1.945)
+big_upperband, big_middleband, big_lowerband = calculate_bollinger(big_df, 20, 1.949)
 
 
 #Prices at points of candles in 1m chart
