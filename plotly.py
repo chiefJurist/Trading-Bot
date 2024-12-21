d5178e1d59949003e386cabcd121c474b2fb7560
chiefJurist <anthonynnanna004@gmail.com>
2024-12-21 14:01:57 +0100

    xczcxzxz

---
+++ plotly.py
@@ -0,0 +1,26 @@
+import plotly.graph_objects as go
+
+# Sample data
+data = {
+    'x': ['2024-12-21 04:27:00', '2024-12-21 04:28:00', '2024-12-21 04:29:00'],
+    'y': [100, 200, 150],
+}
+
+# Create a simple line chart
+fig = go.Figure(data=go.Scatter(x=data['x'], y=data['y'], mode='lines+markers', name='Test Line'))
+
+# Update layout for better visuals
+fig.update_layout(
+    title='Simple Line Chart',
+    xaxis_title='Time',
+    yaxis_title='Value',
+    template='plotly_dark'
+)
+
+# Display the chart
+fig.show()
+
+# Save to HTML
+import plotly.io as pio
+pio.write_html(fig, file='simple_chart.html', auto_open=False)
+print("HTML file saved as 'simple_chart.html'")
