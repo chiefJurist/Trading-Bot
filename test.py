f6c0ced2dd181da9807362daea354b5d87b9aeea
chiefJurist <anthonynnanna004@gmail.com>
2025-01-27 06:31:53 +0100

    sacvcv

--- test.py
+++ test.py
@@ -308,3 +308,7 @@ ohlcv_data2['upperband'], ohlcv_data2['middleband'], ohlcv_data2['lowerband'] = 
 for i in range(2, len(ohlcv_data)):
     # Get the current and previous candles for the 1-minute chart
     print(ohlcv_data['close'].iloc[i])
+
+for i in range(2, len(ohlcv_data)):
+    # Get the current and previous candles for the 5-minute chart
+    print(ohlcv_data2['close'].iloc[i])
