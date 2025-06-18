6a8c65a0aa2b1988b5a907c2203ec5c25be5c30d
Anthony Nnanna <anthonynnanna004@gmail.com>
2025-06-18 05:19:07 +0100

    commit

---
+++ forex_bot.py
@@ -0,0 +1,25 @@
+import ccxt
+import pandas as pd
+import talib as ta
+import time
+import math
+import numpy as np
+
+# The User's API keys and addresses
+SPOT_API_KEY = 'lwObDP3Fcjia0OxQfombVxtG032NZWuFE4ctgKZPxBJYzblviFUd3ONUFAuOwcJq'
+SPOT_SECRET_KEY = 'PIx7RsETsxoqkJUaltX5pssmHnCMSXqUmb0lJv5OHR4RxS9kOtN2UCeYAtqFFAVm'
+FUTURES_API_KEY = 'FGqsynCXzPJoBJvcLG28EkQg2HvWlE4ANzt9Q3IIVH1iCqvuZ3jQh4Ipw2SzcpYn'
+FUTURES_SECRET_KEY = 'Tg0IcgRO3Xh7PhxfSUs6MfheM46uWa7BIydrLy2Q3ker2VWIQlMvHBmiq566EVEz'
+ADDRESS_ONE = '0x9D95d4751fCc02157d55527Ca4D50588bCC80590'
+
+# Initialize the Binance Spot exchange
+binance_spot = ccxt.binance({
+    'apiKey': SPOT_API_KEY,
+    'secret': SPOT_SECRET_KEY,
+})
+
+# Initialize the Binance Futures exchange
+binance_futures = ccxt.binanceusdm({
+    'apiKey': FUTURES_API_KEY,
+    'secret': FUTURES_SECRET_KEY,
+})
